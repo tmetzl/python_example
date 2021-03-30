75b04c46b5a27d1672ce6e7bf0874061ffae9f1a
Tim Metzler <33530562+tmetzl@users.noreply.github.com>
2021-03-31 00:51:17 +0200

    Update test_add.py

--- python_example/tests/example_module/test_add.py
+++ python_example/tests/example_module/test_add.py
@@ -5,10 +5,10 @@ from python_example.example_module import add
 class TestAdd(unittest.TestCase):
 
     def setUp(self):
-        pass
+        pass # code executed before each test
 
     def test_add(self):
         assert add(1, 2) == 3, 'Test for adding numbers failed'
 
     def tearDown(self):
-        pass
+        pass # code executed after each test
